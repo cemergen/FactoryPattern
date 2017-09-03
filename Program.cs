8fb7f9d9c833280217674280bd18c39c96f13898
cem <cemergen@gmail.com>
2017-09-03 21:29:22 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creator creator = new Creator();
+
+            IceCream strawBerry = creator.IceCreamFactory(IceCreamTypes.Strawberry);
+            IceCream vanilla = creator.IceCreamFactory(IceCreamTypes.Vanilla);
+            IceCream[] iceCreamArry = { vanilla, strawBerry };
+            foreach (var item in iceCreamArry)
+            {
+                item.Type();
+            }
+
+        }
+
+        public enum IceCreamTypes
+        {
+            Strawberry,
+            Chocolate,
+            Vanilla
+        }
+
+        public abstract class IceCream
+        {
+            public abstract void Type();
+        }
+
+        public class Strawberry : IceCream
+        {
+            public override void Type()
+            {
+                Console.WriteLine("Strawberry");
+            }
+        }
+        public class Chocolate : IceCream
+        {
+            public override void Type()
+            {
+                Console.WriteLine("Chocolate");
+            }
+        }
+
+        public class Vanilla : IceCream
+        {
+            public override void Type()
+            {
+                Console.WriteLine("Vanilla");
+            }
+        }
+
+        public class Creator
+        {
+            public IceCream IceCreamFactory(IceCreamTypes iceCreamTypes)
+            {
+                IceCream iceCream = null;
+                switch (iceCreamTypes)
+                {
+                    case IceCreamTypes.Strawberry:
+                        iceCream = new Strawberry();
+                        break;
+                    case IceCreamTypes.Chocolate:
+                        iceCream = new Chocolate();
+                        break;
+                    case IceCreamTypes.Vanilla:
+                        iceCream = new Vanilla();
+                        break;
+                }
+                return iceCream;
+            }
+        }
+    }
+}
